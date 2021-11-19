0e34bc2667b3b2c255d0cf54c6dd2b332cf889d5
Lucas Ferrari Soto <lcsfs11@gmail.com>
2021-11-19 13:04:29 -0300

    Fix linting errors

--- app/models/service.rb
+++ app/models/service.rb
@@ -4,5 +4,5 @@ class Service < ApplicationRecord
   validates :duration, presence: true
   validates :price, presence: true
   validates :city, presence: true
-  validates :image_url, presence: true, format: URI::regexp(%w[http https])
+  validates :image_url, presence: true, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
 end
