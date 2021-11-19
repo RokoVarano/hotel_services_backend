13f6a5217969a5d4e064df4a1e39393f770c5cee
Lucas Ferrari Soto <lcsfs11@gmail.com>
2021-11-19 18:29:47 -0300

    Add Service model associations and serialization method

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :user
+  belongs_to :reservation
 
   validates :date, presence: true
   validates :city, presence: true
--- app/models/service.rb
+++ app/models/service.rb
@@ -3,4 +3,17 @@ class Service < ApplicationRecord
   validates :description, presence: true, length: { maximum: 250 }
   validates :price, presence: true
   validates :image_url, presence: true, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
+
+  has_many :reservations
+  has_many :users, through: :reservations
+
+  def as_json(options={})
+    {
+      id: id,
+      name: name,
+      description: description,
+      price: price,
+      image_url: image_url
+    }
+  end
 end
