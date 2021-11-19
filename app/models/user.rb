ab8f8dce0ce255919f3ed2e40cea231af25419be
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2021-11-19 21:58:27 +0100

    Add uniqueness validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { in: 4..20 }
+  validates :name, presence: true, length: { in: 4..20 },
+    uniqueness: true
 
   has_many :reservations
 end
