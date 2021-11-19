9c182ce411c42b6b65b4e0e671715cb9fb111a14
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2021-11-19 18:50:13 +0100

    fixed linter error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { in: 4..20,
-    invalid_length: "%{count} should be between 4 and 20 characters" }
+  validates :name, presence: true, length: { in: 4..20 }
 
-    has_many :reservations
+  has_many :reservations
 end
