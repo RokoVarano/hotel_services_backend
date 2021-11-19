3dbdcefcb2098febc49ccd15cb219f96621a841f
Lucas Ferrari Soto <lcsfs11@gmail.com>
2021-11-19 14:25:20 -0300

    Update Service model to not have dutation or city

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_19_155515) do
+ActiveRecord::Schema.define(version: 2021_11_19_172039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,7 @@ ActiveRecord::Schema.define(version: 2021_11_19_155515) do
   create_table "services", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
-    t.integer "duration", null: false
     t.integer "price", null: false
-    t.string "city", null: false
     t.string "image_url", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
