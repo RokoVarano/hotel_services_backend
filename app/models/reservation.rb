ea203098ac09661d42468bcd1f2648c17d057d13
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2021-11-22 14:39:46 +0100

    add user model methods and association

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,14 @@ class Reservation < ApplicationRecord
 
   validates :date, presence: true
   validates :city, presence: true
+
+  def as_json(options={})
+    {
+      id: id,
+      user_id: user_id,
+      service_id: service_id,
+      date: date,
+      city: city
+    }
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,12 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { in: 4..20 },
     uniqueness: true
 
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
+  has_many :services, through: :reservations
+
+  
+
+  def user_exists?(user_name)
+    User.exists?(name: user_name)
+  end
 end
