bdf3abeb999f74672979b8722703650537b2f206
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2021-11-23 22:25:33 +0100

    added validation test for service model

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,5 @@
 class Service < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: { minimum: 4 }
   validates :description, presence: true, length: { maximum: 250 }
   validates :price, presence: true
   validates :image_url, presence: true, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
---
+++ spec/models/service_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+# RSpec.describe Service, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+
+RSpec.describe Service, :type => :model do
+  subject { 
+         described_class.new(name: 'Sample 1 service', 
+                             description: 'Sample 1 Service description',
+                             price: 4,
+                             image_url: 'https://drive.google.com/file/d/1346McxIF6VhUSCBim7NjxWwU_N-GNkl4/view?usp=sharing'
+         )  
+  }
+
+  describe "Validations" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a name" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid with a name of 'abc'" do
+      subject.name = 'abc'
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a description" do
+      subject.description = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a price" do
+      subject.price = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without an image url" do
+      subject.image_url = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid with a wrongly formatted url" do
+      subject.image_url = 'amazing sljsjljs'
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe "Associations" do
+    
+  end
+end
